aad6901130835d26490f88069ddf8d963ad77bb4
mahbubul14 <mahbub21alam2017@gmail.com>
2022-02-14 22:18:35 +0600

    Delete all files

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root 'users#index'
-end
