244a0091021a5eea071cf9dd00adac680b4c0f6a
mahbubul14 <mahbub21alam2017@gmail.com>
2022-02-24 11:27:51 +0600

    Create navbar with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-Rails.application.routes.draw do
-  # GET /about
-  get "about", to: "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # GET /about
   # Defines the root path route ("/")
   # root "articles#index"
+Rails.application.routes.draw do
+
+  get "about-us", to: "about#index", as: :about
+
+  root to: "main#index"
 end
