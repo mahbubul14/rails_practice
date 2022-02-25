24caee8e840bb0683c2361f0d02418c88bf49c9b
mahbubul14 <mahbub21alam2017@gmail.com>
2022-02-25 10:33:57 +0600

    Add resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root "articles#index"
   # get 'articles/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  root "articles#index"
-  get "articles", to: "articles#index"
+  # get "articles", to: "articles#index"
+  # get "articles/:id", to: "articles#show"
+  resources :articles
 end
