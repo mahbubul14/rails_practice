68fd7311b282834f4cc293bb715277560344bab0
mahbubul14 <mahbub21alam2017@gmail.com>
2022-03-12 23:24:38 +0600

    Delete all folder and files

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root "articles#index"
-  # get 'articles/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # get "articles", to: "articles#index"
-  # get "articles/:id", to: "articles#show"
-  resources :articles
-end
