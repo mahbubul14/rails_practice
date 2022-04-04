2284f35cc14ccd36f391f3cd95e96ecceffd988a
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-04 20:12:04 +0600

    Add rspec test for controller

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get('/users') }
+      it 'http response should be sucess' do
+        expect(response).to have_http_status(:ok)
+      end
+      it 'renders correct page contents' do
+        expect(response.body).to include('List all users')
+      end
+  end
+end
